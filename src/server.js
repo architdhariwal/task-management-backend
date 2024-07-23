8596d828204f9e6510735470378181e4cae4f6d6
architKumar <https://github.com/architdhariwal>
2024-07-23 06:04:51 +0530

    removed cors dependency

--- src/server.js
+++ src/server.js
@@ -10,10 +10,11 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true
-}));
+// app.use(cors({
+//     origin: process.env.FRONTEND_URL,
+//     credentials: true
+// }));
+app.use(cors());
 app.use(express.json());
 app.use(session({
     secret: process.env.SESSION_SECRET,
